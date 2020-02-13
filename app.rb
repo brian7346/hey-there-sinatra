6810d9b3d6f3e7c672b067c36b4a0418d29b405a
maxroslow <brian2009@yandex.ru>
2020-02-13 21:21:24 +0400

    feat: add details view

--- app.rb
+++ app.rb
@@ -58,5 +58,8 @@ end
 get '/details/:post_id' do
   post_id = params[:post_id]
 
-  erb post_id
+  results = @db.execute 'select * from Posts where id=?', [post_id]
+  @row = results[0]
+
+  erb :details
 end
