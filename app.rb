fa5ce2e2fc497e1d02e6a8ac75c7f88dabf9c501
maxroslow <brian2009@yandex.ru>
2020-02-13 19:38:31 +0400

    initial commit

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+
+configure do
+  enable :sessions
+end
+
+helpers do
+  def username
+    session[:identity] ? session[:identity] : 'Hello stranger'
+  end
+end
+
+get '/' do
+  erb 'Hello World!'
+end
